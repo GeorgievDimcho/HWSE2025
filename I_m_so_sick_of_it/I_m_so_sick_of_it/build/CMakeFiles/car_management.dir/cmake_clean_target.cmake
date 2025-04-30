e34198e69c6e21a870c1376d5bdd37d9f02a4b99
Youra <a@a.c>
2025-04-30 22:00:06 +0200

    remove cmock example

--- I_m_so_sick_of_it/I_m_so_sick_of_it/build/CMakeFiles/car_management.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libcar_management.a"
-)
--- I_m_so_sick_of_it/I_m_so_sick_of_it/inc/data/file_handling.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILE_HANDLING_H
-#define FILE_HANDLING_H
-#include "student.h"
-
-int save_student_data(student_t *student);
-
-
-#endif // FILE_HANDLING_H
